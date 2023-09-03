93d68fc8268449ef18d3299830c1804f6c5dea4e
maalroja <maalroja@bancolombia.com.co>
2023-09-02 23:29:30 -0400

    chore: add schema validation request and reply

--- server.js
+++ server.js
@@ -5,8 +5,42 @@ const fastify = Fastify({
 })
 
 // Declare a route
-fastify.get('/', async function handler(request, reply){
-    return { hello: 'world'}
+//fastify.get('/', async function handler(request, reply){
+//    return { hello: 'world'}
+//})
+
+//http://localhost:3000?name=example
+
+fastify.route({
+    method: 'GET',
+    url: '/',
+    schema: {
+        // request needs to have a querystring with a `name` parameter
+        querystring: {
+            type: 'object',
+            properties: {
+                name: { type: 'string' }
+            },
+            required: ['name'],
+        },
+        // the response needs to be an object with a `hello` property of type 'string' 
+        response: {
+            200: {
+                type: 'object',
+                properties: {
+                    hello: { type: 'string' }
+                }
+            }
+        }
+    },
+    // This function is executed for every request before the handler is executed
+    preHandler: async (request, reply) => {
+        // E.g. check authentication
+        console.log('preHandler');
+    },
+    handler: async (request, reply) => {
+        return { hello: 'world' }
+    }
 })
 
 try {
